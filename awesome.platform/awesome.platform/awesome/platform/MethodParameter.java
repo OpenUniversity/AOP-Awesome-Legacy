6547c17eea807b046ea609332a3917fb11ec2141
Oren Mishali <omishali@openu.ac.il>
2011-05-17 09:26:25 +0000

    git-svn-id: svn://trac.cslab.openu.ac.il/svn/awesome@56 792cf01c-dc49-49e2-86e7-086c45b58a0a

---
+++ awesome.platform/awesome.platform/awesome/platform/MethodParameter.java
@@ -0,0 +1,29 @@
+package awesome.platform;
+
+public class MethodParameter {
+	private Type type;
+	private Object value;
+	
+	public enum Type {
+		EnclosingClassName,
+		EnclosingMethodName,
+		Primitive
+	}
+	public MethodParameter(MethodParameter.Type type) {
+		this.type = type;
+	}
+	public MethodParameter(String str) {
+		type = Type.Primitive;
+		value = str;
+	}
+	public MethodParameter(Boolean bool) {
+		type = Type.Primitive;
+		value = bool;
+	}
+	public Type getType() {
+		return type;
+	}
+	public Object getValue() {
+		return value;
+	}
+}
---
+++ awesome.platform/awesome.platform/org/aspectj/apache/bcel/generic/Tag.java
@@ -0,0 +1,43 @@
+/* *******************************************************************
+ * Copyright (c) 2002 Contributors
+ * All rights reserved. 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ * Contributors: 
+ *     PARC     initial implementation 
+ *   Andy Clement   pushed down into bcel module
+ * ******************************************************************/
+
+package org.aspectj.apache.bcel.generic;
+
+/**
+ *  A tag is an instruction-targeter that does not remember its target 
+ */
+public abstract class Tag implements InstructionTargeter, Cloneable {
+
+    public Tag() {
+    }
+
+    // ---- from InstructionTargeter
+    public boolean containsTarget(InstructionHandle ih) {
+        return false;
+    }
+
+    public void updateTarget(InstructionHandle oldHandle, InstructionHandle newHandle) {
+        oldHandle.removeTargeter(this);
+        if (newHandle != null) {
+            newHandle.addTargeter(this);
+        }
+    }
+    
+    public Tag copy() {
+    	try {
+    		return (Tag)clone();
+    	} catch (CloneNotSupportedException e) {
+    		throw new RuntimeException("Sanity check, can't clone me");
+    	}
+    }
+}
